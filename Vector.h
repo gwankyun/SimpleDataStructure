dea0ad330222b90d7bb54260e718f81c84b33e01
gwankyun <ljqican@163.com>
2017-03-02 23:48:42 +0800

    實現Vector

---
+++ Vector.h
@@ -0,0 +1,60 @@
+#pragma once
+
+template<typename T>
+class Vector
+{
+public:
+	Vector(int size) : _room(size), _size(0)
+	{
+		_data = new T[_room];
+	}
+
+	~Vector()
+	{
+		delete[] _data;
+	}
+
+	T get(int index)
+	{
+		return _data[index];
+	}
+
+	T set(int index, T value)
+	{
+		_data[index] = value;
+	}
+
+	void push_back(T value)
+	{
+		if (_size == _room)
+		{
+			extend();
+			_data[_size] = value;
+			_size++;
+		}
+		else
+		{
+			_data[_size] = value;
+			_size++;
+		}
+	}
+
+	int size()
+	{
+		return _size;
+	}
+
+private:
+	T* _data = nullptr;
+	int _size = 0;
+	int _room = 0;
+
+	void extend()
+	{
+		T* newData = new T[_size * 2];
+		_room *= 2;
+		std::copy(_data, _data + _size, newData);
+		delete[] _data;
+		_data = newData;
+	}
+};
